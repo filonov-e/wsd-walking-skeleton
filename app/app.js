b606f4270e4a8a6286a5d00ab3ceadd405ba4892
filonov-e <egor.filonov@smartly.io>
2022-04-18 13:53:11 +0300

    Added changes

--- app/app.js
+++ app/app.js
@@ -1,9 +1,31 @@
 import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
+import { configure, renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts";
+import { extractPerson } from "./utils/formUtils.js";
+import * as peopleService from "./services/peopleService.js";
 
-const handleRequest = (request) => {
-  console.log(`Request to ${request.url}`);
-  return new Response("Hello world!");
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
+
+const responseDetails = {
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
-console.log("Launching server on port 7777");
+const addPerson = async (request) => {
+  const person = await extractPerson(request);
+  peopleService.add(person);
+};
+
+const handleRequest = async (request) => {
+  if (request.method === "POST") {
+    await addPerson(request);
+  }
+
+  const data = {
+    people: peopleService.findAll(),
+  };
+
+  return new Response(await renderFile("index.eta", data), responseDetails);
+};
+
 serve(handleRequest, { port: 7777 });
